de8863eea8dd6cacbd256c714e8d7d387434c76b
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2017-04-04 01:43:46 +0530

    Add support for fixed-height panels

--- src/pf-accordion/pf-accordion.component.js
+++ src/pf-accordion/pf-accordion.component.js
@@ -107,6 +107,14 @@ export class PfAccordion extends HTMLElement {
     });
 
     this._obeserver.observe(this, { childList: true });
+
+    if (this.hasAttribute('fixedheight')) {
+      // _initialized is raised after _initCollapseHeights
+      this._initCollapseHeights();
+    } else {
+      this._initialized = true;
+      this.dispatchEvent(new Event('initialized'));
+    }
   }
 
   /**
@@ -141,7 +149,87 @@ export class PfAccordion extends HTMLElement {
     this._obeserver.disconnect();
   }
 
+
+  /**
+   * Called when element's attribute value has changed
+   *
+   * @param {string} attrName The attribute name that has changed
+   * @param {string} oldValue The old attribute value
+   * @param {string} newValue The new attribute value
+   */
+  attributeChangedCallback (attrName, oldValue, newValue) {
+    if (attrName === 'fixedheight') {
+      if (newValue) {
+        this._initCollapseHeights();
+      }
+    }
+  }
+
+  /**
+  * Recalculates abd sets the collapse height after every browser resize
+  */
+  _setCollapseHeights () {
+    let overflowY = 'hidden';
+
+    let height = this.clientHeight;
+
+    // Close any open panel
+    let openPanels = this.querySelectorAll('.collapse.in');
+
+    Array.prototype.forEach.call(openPanels, (openPanel) => {
+      openPanel.classList.remove('in');
+    });
+
+    // Determine the necessary height for the closed content
+    let contentHeight = 0;
+    Array.prototype.forEach.call(this.children, (element) => {
+      contentHeight += element.offsetHeight;
+    });
+
+    // Determine the height remaining for opened collapse panels
+    let bodyHeight = this.clientHeight - contentHeight;
+
+    // Make sure we have enough height to be able to scroll the contents if necessary
+    if (bodyHeight < 25) {
+      bodyHeight = 25;
+
+      // Allow the parent to scroll so the child elements are accessible
+      overflowY = 'auto';
+    }
+
+    // Reopen the initially opened panel
+    Array.prototype.forEach.call(openPanels, (openPanel) => {
+      openPanel.classList.add('in');
+    });
+
+    requestAnimationFrame( () => {
+    // Set the max-height for the collapse panels
+      let panels = this.getElementsByTagName('pf-accordion-template');
+      Array.prototype.forEach.call(panels,(element) => {
+        // Set the max-height and vertical scroll of the scroll element
+        element.style.maxHeight = bodyHeight + 'px';
+        element.style.overflowY = 'auto';
+      });
+      this.style.overflowY =  overflowY;
+
+      if (!this._initialized) {
+        // first time run, send an initialized event
+        this._initialized = true;
+        this.dispatchEvent(new Event('initialized'));
+      }
+    });
+  }
+
+  /**
+   * initializes a fixed-width accordion
+   */
+  _initCollapseHeights () {
+    this._setCollapseHeights();
+    // Update on window resizing
+    window.addEventListener('resize', this._setCollapseHeights.bind(this));
+  }
 }
+
 (function () {
   document.registerElement('pf-accordion', PfAccordion);
 }());
