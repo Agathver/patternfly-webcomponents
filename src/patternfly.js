bd432b6a2fcfc4b734e28ef3367c3161ec6d363a
Amitosh Swain Mahapatra <amitosh.swain@gmail.com>
2017-05-01 19:37:00 +0530

    Wrap custom element definitions under 'WebComponentsReady'

--- src/patternfly.js
+++ src/patternfly.js
@@ -1,38 +1,41 @@
 /* Require the HTML Element Shim */
 require('./customElementShim.js');
 
-/** PF Accordion Component **/
-require('pf-accordion.component.js');
+/* Wait for polyfill loading */
+document.addEventListener('WebComponentsReady', () => {
+  /** PF Accordion Component **/
+  require('pf-accordion.component.js');
 
-/** PF Alert Component **/
-require('pf-alert.component.js');
+  /** PF Alert Component **/
+  require('pf-alert.component.js');
 
-/** PfListView Component **/
-require('pf-list-view.component.js');
+  /** PfListView Component **/
+  require('pf-list-view.component.js');
 
-/** PfTemplateRepeaterComponent **/
-require('pf-template-repeater.component.js');
+  /** PfTemplateRepeaterComponent **/
+  require('pf-template-repeater.component.js');
 
-/** PfTemplateComponent **/
-require('pf-template.component.js');
+  /** PfTemplateComponent **/
+  require('pf-template.component.js');
 
-/** PF Tabs Component **/
-require('pf-tabs.component.js');
+  /** PF Tabs Component **/
+  require('pf-tabs.component.js');
 
-/** PF Tooltip Component **/
-require('pf-tooltip.component.js');
+  /** PF Tooltip Component **/
+  require('pf-tooltip.component.js');
 
-/** PF Utilization Bar Chart **/
-require('pf-utilization-bar-chart.component.js');
+  /** PF Utilization Bar Chart **/
+  require('pf-utilization-bar-chart.component.js');
 
-/** PF Utils **/
-require('pf-utils.js');
+  /** PF Utils **/
+  require('pf-utils.js');
 
-/** PF I18N **/
-require('pf-i18n.component.js');
+  /** PF I18N **/
+  require('pf-i18n.component.js');
 
-/** PF Hello **/
-require('pf-hello.component.js');
+  /** PF Hello **/
+  require('pf-hello.component.js');
 
-/** PF Dropdown **/
-require('pf-dropdown.component.js');
+  /** PF Dropdown **/
+  require('pf-dropdown.component.js');
+});
